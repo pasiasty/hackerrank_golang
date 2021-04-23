2da9ad8c281fba5471f9f9714c46da5bdd67dd3a
Mariusz Pasek <pasiasty@gmail.com>
2021-04-23 08:57:28 +0200

    dumping only necessary files

--- dump_solution/dump_solution.go
+++ dump_solution/dump_solution.go
@@ -15,8 +15,13 @@ import (
 
 var (
 	fileRegexp             = regexp.MustCompile(`import \((?P<imports>[\s\S]+?)\)(?P<rest>[\s\S]+)`)
-	fileRegexpSingleImport = regexp.MustCompile(`import (?P<imports>\"[\S]+?\")\n(?P<rest>[\s\S]+)`)
+	fileSingleImportRegexp = regexp.MustCompile(`import (?P<imports>\"[\S]+?\")\n(?P<rest>[\s\S]+)`)
 	noImportsRegexp        = regexp.MustCompile(`package.*(?P<rest>[\s\S]+)`)
+
+	structsRegexp = regexp.MustCompile(`type ([A-Z][a-zA-Z0-9]*) struct`)
+	functsRegexp  = regexp.MustCompile(`func ([A-Z][a-zA-Z0-9]*)\(`)
+
+	utilsSymbolRegexp = regexp.MustCompile(`utils\.([A-Z][a-zA-Z0-9]*)`)
 )
 
 func dumpFileWithoutPackage(filename string, imports map[string]interface{}, cw io.Writer) {
@@ -31,10 +36,10 @@ func dumpFileWithoutPackage(filename string, imports map[string]interface{}, cw 
 	restIdx := fileRegexp.SubexpIndex("rest")
 
 	if len(matches) == 0 {
-		matches = fileRegexpSingleImport.FindStringSubmatch(string(ub))
+		matches = fileSingleImportRegexp.FindStringSubmatch(string(ub))
 
-		importsIdx = fileRegexpSingleImport.SubexpIndex("imports")
-		restIdx = fileRegexpSingleImport.SubexpIndex("rest")
+		importsIdx = fileSingleImportRegexp.SubexpIndex("imports")
+		restIdx = fileSingleImportRegexp.SubexpIndex("rest")
 
 		if len(matches) == 0 {
 			matches = noImportsRegexp.FindStringSubmatch(string(ub))
@@ -57,11 +62,54 @@ func dumpFileWithoutPackage(filename string, imports map[string]interface{}, cw 
 	cw.Write([]byte(strings.ReplaceAll(matches[restIdx], "utils.", "")))
 }
 
+func getUsedUtilsSymbols(path string) map[string]interface{} {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	matches := utilsSymbolRegexp.FindAllStringSubmatch(string(b), -1)
+
+	res := make(map[string]interface{})
+
+	for _, m := range matches {
+		res[m[1]] = new(interface{})
+	}
+
+	return res
+}
+
+func libraryShouldBeDumped(filename string, usedUtilsSymbols map[string]interface{}) bool {
+	ub, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read %s: %v", filename, err))
+	}
+
+	matches := structsRegexp.FindAllStringSubmatch(string(ub), -1)
+
+	for _, m := range matches {
+		if _, ok := usedUtilsSymbols[m[1]]; ok {
+			return true
+		}
+	}
+
+	matches = functsRegexp.FindAllStringSubmatch(string(ub), -1)
+
+	for _, m := range matches {
+		if _, ok := usedUtilsSymbols[m[1]]; ok {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	cw := bytes.NewBuffer(nil)
 	imports := make(map[string]interface{})
 
 	imports[`"os"`] = new(interface{})
+
+	usedUtilsSymbols := getUsedUtilsSymbols(path.Join("solution", "solution.go"))
 
 	filepath.Walk("utils", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -70,7 +118,9 @@ func main() {
 		if strings.Contains(info.Name(), "_test.go") || !strings.Contains(info.Name(), ".go") {
 			return nil
 		}
-		dumpFileWithoutPackage(path, imports, cw)
+		if libraryShouldBeDumped(path, usedUtilsSymbols) {
+			dumpFileWithoutPackage(path, imports, cw)
+		}
 		return nil
 	})
 
